854191ea7f4553c40df31ca177cd101c3202c47d
Igor Piskunov <igor.piskunov@apliteni.com>
2023-04-02 09:54:23 +0300

    Update backup file, image uploading, vue cdn link

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const fs = require('fs');
 const path = require('path')
 
 const serviceStatus = require('./utils/service_status')
@@ -14,6 +15,21 @@ const app = express();
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use('/files', express.static('files'))
+app.use('/image', express.static('image'))
+
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const dir = './image';
+    fs.mkdirSync(dir, { recursive: true });
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+const upload = multer({ storage: storage });
 
 const pages = require('./routes/pages')
 const management = require('./routes/management')
@@ -26,6 +42,14 @@ app.use("/pages",pages)
 app.use('/management', management)
 
 app.get('/:id', (req, res) => res.sendFile(path.join(__dirname + '/templates/content.html')))
+
+app.post('/image/upload', upload.single('animalImage'), (req, res) => {
+  if (!req.file) {
+    res.status(400).json({ error: 'No file provided' });
+  } else {
+    res.status(200).json(req.file.filename);
+  }
+});
 
 
 app.listen(PORT, HOST)

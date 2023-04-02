1eb0536547f7a18133dab43ec9c379ce797f78eb
Igor Piskunov <igor.piskunov@apliteni.com>
2023-04-02 10:01:49 +0300

    Add generic basicAuth file with dummy user/pass

---
+++ bin/utils/basicAuth.js
@@ -0,0 +1,22 @@
+module.exports = (req, res, next) => {
+  let auth = req.get("authorization");
+
+  if (!auth) {
+    res.set("WWW-Authenticate", 'Basic realm="Authorization Required"');
+
+    return res.status(401).send("Authorization Required");
+  } else {
+    let credentials = new Buffer(auth.split(" ").pop(), "base64")
+      .toString("ascii")
+      .split(":");
+
+    /* Hardcoded authentication */
+    if (credentials[0] == "admin" && credentials[1] == "123456") {
+      // authorized
+      next();
+    } else {
+      // not authorized
+      return res.status(401).send("Access Denied (incorrect credentials)");
+    }
+  }
+};
